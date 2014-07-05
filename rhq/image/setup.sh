37aa0ca5be9300e7a1aa94c2e4e8fdb95e13fcbd
Gustavo Fernandes <gustavonalle@gmail.com>
2014-07-05 22:24:11 +0100

    Fix pre install hostname

--- rhq/image/setup.sh
+++ rhq/image/setup.sh
@@ -5,21 +5,20 @@ runuser -l postgres -c "createuser -h 127.0.0.1 -p 5432 -U postgres -S -D -R rhq
 runuser -l postgres -c "createdb -h 127.0.0.1 -p 5432 -U postgres -O rhqadmin rhq"
 
 sed -i -e 's/jboss.bind.address=/jboss.bind.address=0.0.0.0/' /rhq-server-4.11.0/bin/rhq-server.properties
+sed -i -e 's/#rhq.storage.seeds=/rhq.storage.seeds=localhost/' /rhq-server-4.11.0/bin/rhq-storage.properties
+sed -i -e 's/#rhq.storage.hostname=/rhq.storage.hostname=localhost/' /rhq-server-4.11.0/bin/rhq-storage.properties
+
 /rhq-server-4.11.0/bin/rhqctl install --start
 
-sed -i -e 's/c1de208601a7/localhost/g' /rhq-server-4.11.0/rhq-storage/conf/*
-sed -i -e 's/c1de208601a7/localhost/g' /rhq-server-4.11.0/bin/*.properties
-
-echo "Waiting for server to initialize"
-IP=$(/sbin/ifconfig  | grep inet | xargs  | awk '{print $2}')
-
-until $(curl --silent -L http://$IP:7080/coregui/login | grep -i Welcome > /dev/null )
-do
-   printf '.'
-   sleep 5
-done
-echo "done."
+#echo "Waiting for server to initialize"
+#until $(curl --silent -L http://localhost:7080/coregui#Dashboards | grep -i Welcome > /dev/null )
+#do
+#   printf '.'
+#   sleep 5
+#done
+#echo "done."
 
 /rhq-server-4.11.0/bin/rhqctl stop 
 runuser -l postgres -c "pg_ctl -m fast stop"
+
 
