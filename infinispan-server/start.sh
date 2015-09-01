d8621ec1a5fb643a404a5ad458017ba1d25724fd
gustavonalle <gustavonalle@gmail.com>
2015-09-01 15:36:24 +0200

    Support for domain mode

--- infinispan-server/start.sh
+++ infinispan-server/start.sh
@@ -2,4 +2,4 @@
 
 IP=$(ip a s | sed -ne '/127.0.0.1/!{s/^[ \t]*inet[ \t]*\([0-9.]\+\)\/.*$/\1/p}')
 
-exec /opt/jboss/infinispan-server/bin/domain.sh -c clustered.xml -Djgroups.bind_addr=$IP -Djboss.bind.address=$IP "$@"
+exec /opt/jboss/infinispan-server/bin/domain.sh -Djgroups.bind_addr=$IP -Djboss.bind.address=$IP "$@"
